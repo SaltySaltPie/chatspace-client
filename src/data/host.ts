ba4b1ecfd3562c0e17a0fab0655ab34641aee714
SaltySaltPie <84831252+SaltySaltPie@users.noreply.github.com>
2022-02-16 22:38:49 +0700

    Update host.ts

--- src/data/host.ts
+++ src/data/host.ts
@@ -1,4 +1,4 @@
-const hostURL = "https://chatspace-jimbui-server.herokuapp.com/";
+const hostURL = "https://chatspace-jimbui-server.herokuapp.com";
 // const hostURL = "https://chatspace-jimbui-server.netlify.app";
 // const hostURL = "http://localhost:3001";
 
